a2f96f5322260b38679876e008812c9427b84332
tanusuke <snst.lab@gmail.com>
2023-06-20 16:23:41 +0900

    :bug: add loading event onVueSetup

--- packages/front/plugins/onVueSetup.ts
+++ packages/front/plugins/onVueSetup.ts
@@ -1,8 +1,9 @@
-import { $auth, $config, $notification } from "@stores";
+import { $auth, $config, $loading, $notification } from "@stores";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("vue:setup", () => {
     $config().call("darkMode");
+    $loading().show();
     $notification().showTooltipOnLoad();
     if ($auth().token.accessToken) {
       $notification().fetchNew();
